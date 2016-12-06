23f295c8e1fe96d4ae89ba9a91fd33299824b694
ushaavuthu <usavuthu@in.ibm.com>
2016-12-06 17:37:30 +0530

    s

--- example02.go
+++ example02.go
@@ -32,7 +32,7 @@ import (
 type SimpleChaincode struct {
 }
 
-func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface) ([]byte, error) {
 	_, args := stub.GetFunctionAndParameters()
 	var A, B string    // Entities
 	var Aval, Bval int // Asset holdings
